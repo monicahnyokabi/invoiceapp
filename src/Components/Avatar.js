e51c051e2383701188d0bcc3150b6fc8d0c8cdbd
monicahnyokabi <wambuinyokabimn@gmail.com>
2023-08-13 17:27:30 +0300

    Invoice Application

---
+++ src/Components/Avatar.js
@@ -0,0 +1,10 @@
+import image from "../alphacles.png";
+import AvatarStyles from "../Styles/AvatarStyles";
+export default function Avatar() {
+  const classes = AvatarStyles();
+  return (
+    <div className={classes.Avatar}>
+      <img src={image} alt="Avatar"></img>
+    </div>
+  );
+}
